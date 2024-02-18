b79c67e0fff1ad95af02768914053042b08c1a51
Christine He <he.yiyang@northeastern.edu>
2024-02-18 16:15:01 -0500

    Project Structure Built

---
+++ world-in-one/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title_container}>
+        Hello
+      </Text>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title_container: {
+    fontSize: 20,
+    alignContent: 'left',
+    justifyContent: 'top',
+  },
+});
